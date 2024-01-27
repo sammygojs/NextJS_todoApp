55f4e921c73e46a3643c0739d4396af64c60f447
Sumit <akoliyasumit@hotmail.com>
2024-01-27 05:12:46 +0000

    project setup

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-center underline">Hello word</h1>
+      <button className="btn">Click me</button>
+    </>
+  );
+}
